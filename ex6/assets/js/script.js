a4d3d56c7bd14949b50e02d7452cfa9b706401e1
Amaury Dotrice <amaurydotrice@gmail.com>
2019-02-11 14:25:19 +0100

    ex5 fin + ex6

---
+++ ex6/assets/js/script.js
@@ -0,0 +1,6 @@
+function valider() {
+    nbr1 = document.getElementById("premier_nombre").value;
+    nbr2 = document.getElementById("deuxieme_nombre").value;
+
+    window.alert("Reste de la division = "+nbr1%nbr2);
+}
