d5dd6db64695c91a492b2e45ad4605aa2a3fc935
Amaury Dotrice <amaurydotrice@gmail.com>
2019-02-14 14:28:18 +0100

    ex01 série 3

---
+++ Série-3/ex01/assets/js/script.js
@@ -0,0 +1,11 @@
+document.getElementById("image1").addEventListener("mouseover", change);
+
+function change(){
+    document.getElementById("image1").src = "assets/img/image1_2.jpg";
+}
+
+document.getElementById("image1").addEventListener("mouseout", rechange);
+
+function rechange() {
+    document.getElementById("image1").src = "assets/img/image1.jpg";
+}
