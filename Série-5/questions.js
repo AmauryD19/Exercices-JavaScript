8785f7587014d6b7969935b691caefaedede3572
AmauryD19 <amaurydotrice@gmail.com>
2019-02-16 12:03:51 +0100

    Série 5

--- Série-5/questions.js
+++ Série-5/questions.js
@@ -12,55 +12,59 @@ var remplaceECar = function (texte) {
     return text;
 }
 var concatString = function (texte1, texte2) {
-
+    return texte1 + texte2;
 }
 var afficherCar5 = function (texte) {
-
+    return texte.substring(4, 5);
 }
 var afficher9Car = function (texte) {
-
+    return texte.substr(0, 9);
 }
 var majusculeString = function (texte) {
-
+    return texte.toUpperCase();
 }
 var minusculeString = function (texte) {
-
+    return texte.toLowerCase();
 }
 var SupprEspaceString = function (texte) {
-
+    return texte.trim();   
 }
 var IsString = function (texte) {
-
+    return typeof texte === "string";
 }
 
 var AfficherExtensionString = function (texte) {
-
+    return texte.substr(texte.lastIndexOf('.') + 1);
 }
 var NombreEspaceString = function (texte) {
-
+    return texte.split(' ').length - 1;
 }
 var InverseString = function (texte) {
-
+    return texte.split("").reverse().join("");
 }
 
 /**
  * Exercices sur les nombres et les caluls mathématiques
  */
 var calculPuissance = function (x, y) {
-
+    return Math.pow(x, y); /*Math.pow (valeur, exposant)*/
 }
 var valeurAbsolue = function (nombre) {
-
+    return Math.abs(nombre); /*Math.abs = valeur absolue de (nombre)*/
 }
 var valeurAbsolueArray = function (array) {
-
+    for (i=0; i<array.length; i++) {
+        array[i]= Math.abs(array[i]); 
+    }
+    return array;
 }
 var sufaceCercle = function (rayon) {
-
+    return Math.round(Math.PI * (Math.pow(rayon, 2))); /*Math.round = valeur arrondie à l'entier le plus proche*/
 }
 var hypothenuse = function (ab, ac) {
-
+    return Math.sqrt(Math.pow(ab, 2) + Math.pow(ac, 2)); /*Math.sqrt racine carrée d'un nombre*/
 }
 var calculIMC = function (poids, taille) {
-
+    tailleSquare= Math.pow(taille, 2);
+    return Number(((poids)/(tailleSquare)).toFixed(2));
 }
--- Série-5/questions.spec.js
+++ Série-5/questions.spec.js
@@ -4,82 +4,82 @@ describe('Les chaines de caractères : ', function () {
 
     it('Donner la taille de la chaine de caractères', function () {
         var result = tailleString('Ce texte à une certaine taille');
-        expect(result).toEqual(30);
+        expect(result).toEqual(30); /*Done*/
     });
 
     it('Remplacer le premier e de la chaine par un espace', function () {
         var result = remplaceECar('Ce texte à une certaine taille');
-        expect(result).toEqual('C  texte à une certaine taille');
+        expect(result).toEqual('C  texte à une certaine taille'); /*Done*/
     });
 
     it('Concatener les deux chaines de caractères', function () {
         var result = concatString('Le javascript est ', 'super');
-        expect(result).toEqual('Le javascript est super');
+        expect(result).toEqual('Le javascript est super'); /*Done*/
     });
 
     it('Afficher le cinquième caractère de la chaine', function () {
         var result = afficherCar5('Ces exercices sont super!');
-        expect(result).toEqual('e');
+        expect(result).toEqual('e'); /*Done*/
     });
 
     it('Afficher les 9 premiers caractères', function () {
         var result = afficher9Car('Je dirait même qu\'ils sont excellent');
-        expect(result).toEqual('Je dirait');
+        expect(result).toEqual('Je dirait'); /*Done*/
     });
 
     it('Mettre en majuscule la chaine', function () {
         var result = majusculeString('Un autre excellent exercice');
-        expect(result).toEqual('UN AUTRE EXCELLENT EXERCICE');
+        expect(result).toEqual('UN AUTRE EXCELLENT EXERCICE'); /*Done*/
     });
 
     it('Mettre en minuscule la chaine', function () {
         var result = minusculeString('Celui ci aussi!');
-        expect(result).toEqual('celui ci aussi!');
+        expect(result).toEqual('celui ci aussi!'); /*Done*/
     });
     it('Supprimer les espaces avant et après la chaine', function () {
         var result = SupprEspaceString(' Rome ne s\'est pas construite en un jour ');
-        expect(result).toEqual('Rome ne s\'est pas construite en un jour');
+        expect(result).toEqual('Rome ne s\'est pas construite en un jour'); /*Done*/
     });
     it('Afficher true si le parametre d\'entrée de la fonction est de type string', function () {
-        var result = IsString('Est ce une chaine de caractères ?');
-        expect(result).toEqual(true);
+        var result = IsString('Est ce une chaine de caractères ?'); 
+        expect(result).toEqual(true); /*Done*/
     });
     it('Afficher l\'extension du fichier', function () {
         var result = AfficherExtensionString('images/photo01.jpg');
-        expect(result).toEqual('jpg');
+        expect(result).toEqual('jpg'); /*Done*/
     });
     it('Compter le nombre d\'espace dans la chaine', function () {
         var result = NombreEspaceString('Sire ouvrez on en a gros!');
-        expect(result).toEqual(5);
+        expect(result).toEqual(5); /*Done*/
     });
     it('Inverser une chaine de caractères', function () {
         var result = InverseString('Après demain, à partir d\'aujourd\'hui?');
-        expect(result).toEqual('?iuh\'druojua\'d ritrap à ,niamed sèrpA');
+        expect(result).toEqual('?iuh\'druojua\'d ritrap à ,niamed sèrpA'); /*Done*/
     });
 })
 describe('Les nombres et opérations mathématiques : ', function () {
     it('Calculer la puissance d\'un nombre par rapport à un autre (x à la puissance y)', function () {
         var result = calculPuissance(2, 3);
-        expect(result).toEqual(8);
+        expect(result).toEqual(8); /*Done*/ 
     });
     it('Afficher la valeur absolue d\'un nombre', function () {
         var result = valeurAbsolue(-5);
-        expect(result).toEqual(5);
+        expect(result).toEqual(5); /*Done*/
     });
     it('Afficher les valeurs absolues de plusieurs nombres', function () {
         var result = valeurAbsolueArray([-5,-50,-25,-568]);
-        expect(result).toEqual([5,50,25,568]);
+        expect(result).toEqual([5,50,25,568]); /*Done*/
     });
     it('Calculer la surface d\'un cercle en fonction de son rayon. L\'arondir à l\'entier le plus proche', function () {
         var result = sufaceCercle(5);
-        expect(result).toEqual(79);
+        expect(result).toEqual(79); /*Done*/
     });
     it('Calculer l\'hypthènuse d\'un triangle rectangle', function () {
         var result = hypothenuse(5, 8);
-        expect(result).toEqual(9.433981132056603);
+        expect(result).toEqual(9.433981132056603); /*Done*/
     });
     it('Calculer l\'IMC (Poids / (taille x taille).Ne garder que deux chiffres après la virgule.', function () {
         var result = calculIMC(65, 1.75);
-        expect(result).toEqual(21.22);
+        expect(result).toEqual(21.22); /*Done*/
     });
 })
